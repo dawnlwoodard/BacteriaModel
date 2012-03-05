59ec55fcda2c05dac5075c4f7d354f24f41ac547
Jay Phillips <rocketman8473@gmail.com>
2012-03-04 20:50:56 -0500

    fixed printf typing error

--- Bacteria.cpp
+++ Bacteria.cpp
@@ -15,7 +15,7 @@ Bacteria::Bacteria()
 	bacteria.push_back( new Bacterium() );
 	bacteria.push_back( new Bacterium() );
 	bacteria.push_back( new Bacterium() );
-	printf("Number of bacteria: %d\n", bacteria.size());
+	printf("Number of bacteria: %d\n", (int)bacteria.size());
 
 }
 
